c9854534c05dcc10146fd85d6aea8d691566ed76
MatGalos <mateusz.galos@microsoft.wsei.edu.pl>
2020-10-08 16:24:36 +0200

    koniec zajęć

--- ćwiczenie1/aaa.js
+++ ćwiczenie1/aaa.js
@@ -1,5 +1,25 @@
-const a=10;
+//Pobranie referencji
 
-if(a){
-    console.log(a);
+const gallery=document.querySelectorAll('.gallery img');
+const galleryCount=gallery.length;
+//zapisanie się na zdarzenie click
+for(let idx=0;idx<gallery.length;idx++){
+    const img=gallery[idx];
+    img.addEventListener('click',showLightbox);
 }
+//firstImage.addEventListener('click',showLightbox);
+//firstImage.addEventListener('click',hideLightbox);
+
+function showLightbox(ev){
+    console.log(ev.target);
+    const lightbox=document.querySelector('.lightbox');
+    const img=document.querySelector('.lightbox img');
+    const imgUrl=ev.target.src;
+    img.src=imgUrl;
+    lightbox.classList.add('visible');
+}
+
+/*function hideLightbox(){
+    const lightbox=document.querySelector('.visible');
+    lightbox.classList.remove('visible');
+}*/
