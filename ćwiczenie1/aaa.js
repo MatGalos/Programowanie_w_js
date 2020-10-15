66669fcdb82e6b393aed472b87f2a8f6b6142371
MatGalos <67073319+MatGalos@users.noreply.github.com>
2020-10-15 13:09:55 +0200

    added function to scroll through images

--- ćwiczenie1/aaa.js
+++ ćwiczenie1/aaa.js
@@ -1,25 +1,64 @@
 //Pobranie referencji
-
 const gallery=document.querySelectorAll('.gallery img');
 const galleryCount=gallery.length;
+var currentImg
 //zapisanie się na zdarzenie click
 for(let idx=0;idx<gallery.length;idx++){
     const img=gallery[idx];
     img.addEventListener('click',showLightbox);
+    img.setAttribute('id','b'+idx);
 }
-//firstImage.addEventListener('click',showLightbox);
-//firstImage.addEventListener('click',hideLightbox);
 
 function showLightbox(ev){
-    console.log(ev.target);
+    currentImg=ev.target;
     const lightbox=document.querySelector('.lightbox');
     const img=document.querySelector('.lightbox img');
     const imgUrl=ev.target.src;
     img.src=imgUrl;
+    img.setAttribute('onclick','hideLightbox()');
+    lightbox.classList.add('visible');
+
+
+}
+function hideLightbox(){
+    const lightbox=document.querySelector('.lightbox');
+    lightbox.classList.remove('visible');
+    lightbox.removeAttribute('onclick','hideLightbox');
+}
+
+function changeLightbox(change){
+    let calcNewImg=change;
+    if(calcNewImg>gallery.length-1){
+        calcNewImg=0;
+    }
+    if(calcNewImg<0){
+        calcNewImg=gallery.length-1;
+    }
+    const obraz=document.querySelector('#b'+calcNewImg);
+    currentImg=obraz;
+    const lightbox=document.querySelector('.lightbox');
+    const img=document.querySelector('.lightbox img');
+    const imgUrl=obraz.src;
+    img.src=imgUrl;
+    img.setAttribute('onclick','hideLightbox()');
     lightbox.classList.add('visible');
 }
 
-/*function hideLightbox(){
-    const lightbox=document.querySelector('.visible');
-    lightbox.classList.remove('visible');
-}*/
+function leftright(number){
+    let g=showId()+number;
+    changeLightbox(showId()+number);
+}
+
+function showId(){
+    let Id;
+    for(let a=0;a<gallery.length;a++){
+        const b='b'+a;
+        const c=currentImg.id;
+        if(b==currentImg.id){
+            Id=a;
+        }
+    }
+    return Id;
+}
+
+// można zrobić dzięki przypisanemu id
