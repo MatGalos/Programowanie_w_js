404be4100534dfab029f0d8eac88b76e92d94bbb
MatGalos <67073319+MatGalos@users.noreply.github.com>
2020-11-16 17:15:35 +0100

    buttons (style, functionality)

--- Sound Kit/main.js
+++ Sound Kit/main.js
@@ -1,40 +1,80 @@
-document.body.addEventListener('keypress',onkeypress)
-document.querySelector('#recordBtn').addEventListener('click'.onRecordButton());
-document.querySelector('#playBtn').addEventListener('click'.onPlayButton());
+document.body.addEventListener('keypress', onKeyPress);
+document.querySelector('#recordBtn').addEventListener('click', onRecordBtnClick);
+document.querySelector('#playBtn').addEventListener('click', onPlayBtnClick);
+document.querySelectorAll('.keycaps').forEach(button=>{
+    button.addEventListener('transitionend',removeTransition);
+})
 
-let recordedSound=[];
 let recordStartTime;
+const recordedSounds = [];
 
-function onkeypress(ev){
-    let soundId;
-    switch (ev.code){
-        case 'KeyA':
-            soundId='boom';
-            break;
-        case 'KeyS':
-            soundId='clap';
-            break;
-        case 'KeyD':
-            soundId='hihat';
-            break;
-        case 'KeyF':
-            soundId='kick';
-            break;
+function removeTransition(ev){
+    console.log(ev.propertyName);
+    if(ev.propertyName==='transform') this.classList.remove('playing');
+}
+
+function onKeyPress(ev) {
+    let soundName;
+    const key=document.querySelector(`div[data-key="${ev.code}"]`);
+    switch (ev.code) {
+    case 'KeyA':
+        soundName = 'boom';
+        break;
+    case 'KeyS':
+        soundName = 'clap';
+        break;
+    case 'KeyD':
+        soundName = 'hihat';
+        break;
+    case 'KeyF':
+        soundName = 'kick';
+        break;
+    case 'KeyG':
+        soundName = 'openhat';
+        break;
+    case 'KeyH':
+        soundName = 'ride';
+        break;
+    case 'KeyJ':
+        soundName = 'snare';
+        break;
+    case 'KeyK':
+        soundName = 'tink';
+        break;
+    case 'KeyL':
+        soundName = 'tom';
+        break;
     }
-    if(soundId){
-        const soundTime=Date.now()-recordStartTime;
-        const sound=document.querySelector('#'+soundId);
-        const soundObj=[soundId,soundTime]
-        sound.play();
-        recordedSound.push(soundObj);
+    if (soundName) {
+        const soundTime = Date.now() - recordStartTime;
+        const soundObj = {
+            id: soundName, 
+            time: soundTime
+        };
+        recordedSounds.push(soundObj);
+        key.classList.add('playing');
+        playSound(soundName);
     }
 }
 
-function onRecordButton(){
-    recordedSound=Date.now;
+function onRecordBtnClick() {
+    recordStartTime = Date.now();
+}
+function onPlayBtnClick() {
+    for (let index = 0; index < recordedSounds.length; index++) {
+        const soundObj = recordedSounds[index];
+        setTimeout(
+            () => {
+                playSound(soundObj.id);
+            },
+            soundObj.time
+        );
+        
+    }
 }
 
-function onPlayButton(){
-    const soundId=recordedSound[5][0];
-    const soundTime=recordedSound[5][1];
+function playSound(id) {
+    const sound = document.querySelector('#' + id);
+    sound.currentTime = 0;
+    sound.play();
 }
